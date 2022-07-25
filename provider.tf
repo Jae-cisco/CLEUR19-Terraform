8f7a8c4f79d19063533f7478557b3edc2c5cba60
Jae-cisco <76939023+Jae-cisco@users.noreply.github.com>
2022-07-25 17:09:03 +0900

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aci = {
+      source = "CiscoDevNet/aci"
+      version = "2.4.0"
+    }
+  }
+}
+
+provider "aci" {
+  # cisco-aci user name
+  username = var.username
+  # cisco-aci password
+  password = var.password
+  # cisco-aci url
+  url      = var.url
+  insecure = true
+  #proxy_url = "https://proxy_server:proxy_port"
+}
