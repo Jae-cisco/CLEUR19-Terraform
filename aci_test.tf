e3dbc16cb67117745880bd19ecfe11c2dbd6629a
Jae-cisco <76939023+Jae-cisco@users.noreply.github.com>
2022-07-25 17:12:07 +0900

    Update aci_test.tf

--- aci_test.tf
+++ aci_test.tf
@@ -1,23 +1,3 @@
-terraform {
-  required_providers {
-    aci = {
-      source = "CiscoDevNet/aci"
-      version = "2.4.0"
-    }
-  }
-}
-
-provider "aci" {
-  # cisco-aci user name
-  username = var.username
-  # cisco-aci password
-  password = var.password
-  # cisco-aci url
-  url      = var.url
-  insecure = true
-  #proxy_url = "https://proxy_server:proxy_port"
-}
-
 resource "aci_tenant" "terraform_ten" {
   name = "jaheo-terraform-test"
 }
