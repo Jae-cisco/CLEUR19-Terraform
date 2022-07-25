40e581b92dd9c6a5b292a6f2e65c3f46e4fefa69
Jae-cisco <76939023+Jae-cisco@users.noreply.github.com>
2022-07-25 17:02:11 +0900

    Update aci_test.tf

--- aci_test.tf
+++ aci_test.tf
@@ -1,8 +1,9 @@
 terraform {
+  required_version = ">= 1.0.0"
   required_providers {
     aci = {
-      source = "CiscoDevNet/aci"
-      version = "2.3.0"
+      source  = "CiscoDevNet/aci"
+      version = "~> 0.7.0"
     }
   }
 }
