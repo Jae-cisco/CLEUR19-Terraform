d6e20e3fa2cde45b166a23ceb58e05db25bab0b3
Jae-cisco <76939023+Jae-cisco@users.noreply.github.com>
2022-07-26 10:50:36 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,10 @@
 terraform {
+  required_version = ">= 1.0.0"
+
   required_providers {
     aci = {
-      source = "CiscoDevNet/aci"
-      version = "2.4.0"
+      source  = "CiscoDevNet/aci"
+      version = "~> 0.9.0"
     }
   }
 }
